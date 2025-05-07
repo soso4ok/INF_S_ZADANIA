3c3c1f5c7aac132efb65236fe780f970da873edc
soso4ok <nxdgbccvbnnnn@gmail.com>
2025-05-07 16:11:25 +0200

    Dataset preparation

--- src/main/java/com/vova/study/ParallelSorter.java
+++ src/main/java/com/vova/study/ParallelSorter.java
@@ -1,4 +1,39 @@
 package com.vova.study;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutionException;
+
+
+/**
+ * Parallel sorting of a list of integers using a divide-and-conquer approach.
+ * The list is partitioned into smaller sublists, each sorted concurrently using multiple threads,
+ * and the sorted sublists are merged to produce the final sorted list
+ */
 public class ParallelSorter {
+    public List<Integer> parallelSort(List<Integer> list, int numPartitions) {
+
+        // Validation
+        if (list == null || list.isEmpty()) {
+            return new ArrayList<>();
+        }
+        if (numPartitions <= 0) {
+            throw new IllegalArgumentException("Number of partitions must be positivee");
+        }
+        if (numPartitions == 1 || list.size() < numPartitions * 2) {
+            //return sequentialSort(list);
+        }
+
+
+
+        return null;
+    }
+
+
 }

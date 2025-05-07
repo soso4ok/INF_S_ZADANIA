54afd7cc7d997ce096297004875fb6a33ae6ae25
soso4ok <nxdgbccvbnnnn@gmail.com>
2025-05-07 15:48:56 +0200

    Dataset preparation

--- src/main/java/com/vova/study/DataGenerator.java
+++ src/main/java/com/vova/study/DataGenerator.java
@@ -1,4 +1,24 @@
 package com.vova.study;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class DataGenerator {
+
+    /**
+     * Method generate random numbers
+     *
+     * @param size  size of list
+     * @param bound maximum absolute value of numbers
+     */
+
+    public List<Integer> generateRandomList(int size, int bound) {
+        List<Integer> list = new ArrayList<>(size);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            list.add(random.nextInt(bound * 2) - bound);
+        }
+        return list;
+    }
 }

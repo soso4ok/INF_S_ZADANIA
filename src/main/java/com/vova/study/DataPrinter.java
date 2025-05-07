bcbb35e06a3696b7c6c0939bb9b1b10ff0b1e7ae
soso4ok <nxdgbccvbnnnn@gmail.com>
2025-05-07 16:19:29 +0200

    added: print methods

--- src/main/java/com/vova/study/DataPrinter.java
+++ src/main/java/com/vova/study/DataPrinter.java
@@ -1,4 +1,22 @@
 package com.vova.study;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DataPrinter {
+    public void printHeader(String header) {
+
+    }
+
+    public void printProcessors(int processors) {
+
+    }
+
+    public void printProcessingStart(String description, List<Integer> data) {
+
+    }
+
+    public void printProcessingResult(List<Integer> sortedData, long durationMs) {
+
+    }
 }
